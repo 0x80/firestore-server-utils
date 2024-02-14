0a1993036bc3aebde3974fa981aefefe39b26810
Thijs Koerselman <thijskoerselman@gmail.com>
2024-02-14 09:40:31 +0100

    Remove field value helpers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from "./constants";
 export * from "./document";
 export * from "./documents";
-export * from "./field-values";
 export * from "./process-in-chunks";
 export * from "./query-and-process";
