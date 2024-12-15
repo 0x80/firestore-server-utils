469846314f8775e240873b514f2630302e43dc7f
Thijs Koerselman <thijskoerselman@gmail.com>
2024-12-15 11:35:48 +0100

    Add get-document-data family

---
+++ src/documents/get-document-data.ts
@@ -0,0 +1,63 @@
+import type {
+  CollectionReference,
+  Transaction,
+} from "firebase-admin/firestore";
+import { invariant } from "~/helpers";
+
+export async function getDocumentData<T>(
+  collectionRef: CollectionReference,
+  documentId: string
+) {
+  const doc = await collectionRef.doc(documentId).get();
+
+  invariant(
+    doc.exists,
+    `No document available at ${collectionRef.path}/${documentId}`
+  );
+
+  return doc.data() as T;
+}
+
+export async function getDocumentDataMaybe<T>(
+  collectionRef: CollectionReference,
+  documentId?: string | null
+) {
+  if (!documentId) return;
+
+  const doc = await collectionRef.doc(documentId).get();
+
+  if (!doc.exists) return;
+
+  return doc.data() as T;
+}
+
+export async function getDocumentDataFromTransaction<T>(
+  transaction: Transaction,
+  collectionRef: CollectionReference,
+  documentId: string
+) {
+  const doc = await transaction.get(collectionRef.doc(documentId));
+
+  invariant(
+    doc.exists,
+    `No document available at ${collectionRef.path}/${documentId}`
+  );
+
+  return doc.data() as T;
+}
+
+export async function getDocumentDataFromTransactionMaybe<T>(
+  transaction: Transaction,
+  collectionRef: CollectionReference,
+  documentId?: string | null
+) {
+  if (!documentId) return;
+
+  const doc = await transaction.get(collectionRef.doc(documentId));
+
+  if (!doc.exists) {
+    return;
+  }
+
+  return doc.data() as T;
+}
