29b260cac104d7ed2c0fc5649e3cd8a6a080b3d6
Thijs Koerselman <thijskoerselman@gmail.com>
2024-12-15 11:27:18 +0100

    Add function for making documents human-readable

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,6 +3,7 @@ export * from "./get";
 export * from "./get-error-message";
 export * from "./invariant";
 export * from "./is-empty";
+export * from "./is-plain-object";
 export * from "./is-present";
 export * from "./last";
 export * from "./make-wait";
