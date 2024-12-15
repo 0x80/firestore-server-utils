61e0d82616bc0227b627053b3605a6218e596436
Thijs Koerselman <thijskoerselman@gmail.com>
2024-12-15 11:58:07 +0100

    Make the package pure ESM

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  target: "es2022",
+  format: "esm",
+  target: "esnext",
   sourcemap: true,
 });
